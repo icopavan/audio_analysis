8027373faeb0d1a8b1b2f11159be4ccfe2446955
Morgan Prior <morgan@amaras-tech.co.uk>
2013-09-19 20:46:59 +0100

    Basic Window FFT Calculation

---
+++ package/+audio_analysis/calc_fft.m
@@ -0,0 +1,12 @@
+function y = calc_fft(data, fftsize)
+  if (nargin < 2)
+    error('calc_fft:args Usage is calc_fft(data, fftsize)');
+  end
+  [fftsize, datasize] = calc_fft_size(size(data,1), struct('fft_size', fftsize));
+  
+%  Create separate windowing utility which can added as requirement.
+%  http://www.mathworks.co.uk/help/dsp/ref/windowfunction.html
+  window        = blackman(fftsize);
+  windowed_data = window.*data(1:datasize);
+  y             = fft(windowed_data, fftsize);
+end %calc_fft
