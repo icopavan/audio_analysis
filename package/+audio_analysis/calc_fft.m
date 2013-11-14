609e0f4db49503828a9d4519bef6f5b481104e04
Morgan Prior <morgan.prior@gmail.com>
2013-11-14 20:30:24 +0000

    Refactored fft normalisation to handle other window types in the future. Output now includes the FFT and power spectrum

--- package/+audio_analysis/calc_fft.m
+++ package/+audio_analysis/calc_fft.m
@@ -1,15 +1,22 @@
-function y = calc_fft(data, fftsize)
+function [norm_fft_result, norm_power_spec] = calc_fft(data, fftsize)
   if (nargin < 2)
     error('calc_fft:args Usage is calc_fft(data, fftsize)');
   end
   [fftsize, datasize] = audio_analysis.calc_fft_size(size(data,1), struct('fft_size', fftsize));
   
-%  Create separate windowing utility which can added as requirement.
-%  http://www.mathworks.co.uk/help/dsp/ref/windowfunction.html
-  window        = ones(fftsize, 1);
-  windowed_data = window.*data(1:fftsize); 
-
-  %% Perform FFT, Normalise then take magnitude of complex result
-  %    Multiply by 2, because of the DFT-symmetry
-  y             = 2 * abs( fft(windowed_data, fftsize)/fftsize );
+  %% If data oversized remove offset
+  if length(data) > fftsize
+    data          = data(end-fft_size+1:end);
+  end
+  
+   w.window              = ones(fftsize, 1);
+   w.incoherent_power_gain = fftsize;
+   windowed_data         = (data).*(w.window);  
+   
+   %% Perform FFT, Normalise then take magnitude of complex result
+   fft_result            = abs( fft(windowed_data) );
+   power_spectrum        = fft_result.^2  ;  
+  
+   norm_power_spec       = (4 * power_spectrum ) / (w.incoherent_power_gain*fftsize);
+   norm_fft_result       = sqrt(norm_power_spec);
 end 
